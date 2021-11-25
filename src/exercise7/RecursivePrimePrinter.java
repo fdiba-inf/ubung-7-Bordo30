72edb70b2dca5c7bf9c394fa0b0014df92aabf54
Bordo30 <boris.dobrev30@gmail.com>
2021-11-25 13:28:14 +0200

    Let's go!

--- src/exercise7/RecursivePrimePrinter.java
+++ src/exercise7/RecursivePrimePrinter.java
@@ -13,11 +13,26 @@ public class RecursivePrimePrinter {
     }
 
     public static void printPrimes(int n) {
-        // Implement recursive method
+        if(n <= 0) {
+            return;
+        }
+
+        if(isPrime(n)) {
+            System.out.println(n);
+        }
+
+        printPrimes(n-1);
     }
 
     public static boolean isPrime(int n) {
-        // Implement method
-        return false;
+        int divider = 2;
+        boolean primeNumber = true;
+        while(divider <= n/2) {
+            if(n%divider == 0) {
+                primeNumber = false;
+            }
+            divider++;
+        }
+        return primeNumber;
     }
 }
